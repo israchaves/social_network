167ef5f9fc8befc9495b4c2e2914e89087c51953
Israel Chaves Arbaiza <isracharbaiza@gmail.com>
2021-02-21 00:12:01 -0600

    ejemplo

---
+++ ejemplo/materials/twitter/config.py
@@ -0,0 +1,6 @@
+# These are the tokens obtained from https://apps.twitter.com/
+
+CONSUMER_KEY = "WrHtBTDevNUsHMp3LXUcHyXmY"           # Consumer Key (API Key)
+CONSUMER_SECRET = "5z97fGTk6ZARkqjC2JE8KiOoisoNqdtwsDJc849cykOzfDtWfZ"        # Consumer Secret (API Secret)
+ACCESS_TOKEN = "2603937499-kvSO6ZLH58ZgsgFqj6aOZ5xsp7FGmoOA3iRN0bi"           # Access Token
+ACCESS_SECRET = "pTcoERFIchndgErqQqYtcn0EMH669wA74NggAIMusUtzE"          # Access Token Secret
